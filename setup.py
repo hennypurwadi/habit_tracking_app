59387dcf691aed5689d4083c85d48cd0b68b5a87
hennypurwadi <85436090+hennypurwadi@users.noreply.github.com>
2021-09-05 18:55:55 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name='habit_tracker12345',
+        version='1.0.0',
+        description='Habit_Tracker',
+        url='https://github.com/hennypurwadi/habit_tracker12345',
+        author='Henny Purwadi',
+        author_email='hennypurwadiblom@gmail.com',
+        license='Unlicense'
+)
